5677da878ebf272e3a156a0896ef90385dd52a4a
Mohan <mohanvjm@outlook.com>
2025-08-29 21:04:13 +0530

    error handling

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,13 @@
 // Prevent console window in addition to Slint window in Windows release builds when, e.g., starting the app via file manager. Ignored on other platforms.
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use std::error::Error;
 use std::time::Duration;
 use std::thread;
 use pcsc::{Context, Scope, ShareMode, Protocols, Error};
 
 slint::include_modules!();
 
-fn main() -> Result<(), Box<dyn Error>> {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let ui = AppWindow::new()?;
     ui.run()?;
     println!("TokenFlow ACR122U Test");
